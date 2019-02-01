7f808888b537f54867f89da1122108a30ba15ab1
st3824 <st3824@CSELAB01-16.bennett.edu.in>
2019-02-01 09:49:55 +0530

    New Sort

--- src/NewSort.java
+++ src/NewSort.java
@@ -1,9 +1,14 @@
 
-public class NewSort {
+import java.util.Arrays; 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class NewSort { 
+	public static void main(String[] args) 
+	{ 
+		int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 }; 
 
-	}
+		Arrays.sort(arr); 
 
-}
+		System.out.printf("Modified arr[] : %s", 
+						Arrays.toString(arr)); 
+	} 
+} 
